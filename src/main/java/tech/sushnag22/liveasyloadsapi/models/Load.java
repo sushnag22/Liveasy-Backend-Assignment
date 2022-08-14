6f7fbf03b724ef8f908830db49bf5b44c6a8279c
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-14 23:44:24 +0530

    Add model Load.java class with attributes, no args & all args constructors, getter & setters

---
+++ src/main/java/tech/sushnag22/liveasyloadsapi/models/Load.java
@@ -0,0 +1,120 @@
+package tech.sushnag22.liveasyloadsapi.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Table(name = "load")
+@Entity
+public class Load {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String loadingPoint;
+    private String unloadingPoint;
+    private String productType;
+    private String truckType;
+    private int noOfTrucks;
+    private int weight;
+    private String comment;
+    private String shipperId;
+    private LocalDate date;
+
+    public Load() {
+    }
+
+    public Load(int id, String loadingPoint, String unloadingPoint, String productType,
+                String truckType, int noOfTrucks, int weight, String comment, String shipperId, LocalDate date) {
+        this.id = id;
+        this.loadingPoint = loadingPoint;
+        this.unloadingPoint = unloadingPoint;
+        this.productType = productType;
+        this.truckType = truckType;
+        this.noOfTrucks = noOfTrucks;
+        this.weight = weight;
+        this.comment = comment;
+        this.shipperId = shipperId;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoadingPoint() {
+        return loadingPoint;
+    }
+
+    public void setLoadingPoint(String loadingPoint) {
+        this.loadingPoint = loadingPoint;
+    }
+
+    public String getUnloadingPoint() {
+        return unloadingPoint;
+    }
+
+    public void setUnloadingPoint(String unloadingPoint) {
+        this.unloadingPoint = unloadingPoint;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getTruckType() {
+        return truckType;
+    }
+
+    public void setTruckType(String truckType) {
+        this.truckType = truckType;
+    }
+
+    public int getNoOfTrucks() {
+        return noOfTrucks;
+    }
+
+    public void setNoOfTrucks(int noOfTrucks) {
+        this.noOfTrucks = noOfTrucks;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getShipperId() {
+        return shipperId;
+    }
+
+    public void setShipperId(String shipperId) {
+        this.shipperId = shipperId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+}
