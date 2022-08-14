9bb95829d5343cea4aa7ac488b8e4c46172b3329
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-14 23:22:36 +0530

    Initial commit

---
+++ src/main/java/tech/sushnag22/liveasyloadsapi/LiveasyLoadsApiApplication.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.liveasyloadsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyLoadsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveasyLoadsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sushnag22/liveasyloadsapi/LiveasyLoadsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.liveasyloadsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveasyLoadsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
